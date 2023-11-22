fcc517691229a156af7908e9070b5ee17b3a981c
Sergey Makletsov <smaklets@kpfu.ru>
2023-11-22 17:03:03 +0300

    Добавление новой функции

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
+fun oneMoreFun(){
+    println("This is another function")
+}
+
 fun main() {
     println("Hi!")
     println("И снова здравствуйте!")
     println("Просто для еще одного коммита")
+    oneMoreFun()
 }
