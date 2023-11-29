5138a437b507edf3c85fce46d10c951cdbea6072
kiva104 <kira.ivanova.140903@gmail.com>
2023-11-29 11:02:08 +0300

    Выставления требований

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,10 +6,16 @@ fun oneMoreFun(){
     println("Еще функция")
 }
 
+fun myFunK(){
+    println("Я хочу играть в Тайного Санту!!!!")
+    println("(с) Кира :)")
+}
+
 fun main() {
     println("Hi!")
     println("И снова здравствуйте!")
     println("Просто для еще одного коммита")
     oneMoreFun()
     oneMoreFunction2()
+    myFunK()
 }
